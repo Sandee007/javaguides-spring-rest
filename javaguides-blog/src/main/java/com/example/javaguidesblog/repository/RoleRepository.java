cc2e5699abd41ab263344adefab1065512d7f1fc
Sandee007 <sandeepaalahakoon@gmail.com>
2023-10-16 19:06:48 +0530

    user,role repos

---
+++ javaguides-blog/src/main/java/com/example/javaguidesblog/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.example.javaguidesblog.repository;
+
+import com.example.javaguidesblog.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
